d9cba0687743cc2f74253122df5731268dd9e1a3
digola <digolanet@gmail.com>
2025-10-19 17:46:21 -0300

    teste supabe
    exibir console.log

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -22,7 +22,7 @@ export const createClient = async () => {
             // The `setAll` method was called from a Server Component.
             // This can be ignored if you have middleware refreshing
             // user sessions.
-            console.log("vai se fuder)
+            console.log("vai se fuder")
           }
         },
       },
